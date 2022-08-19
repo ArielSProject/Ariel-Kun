54ddea7e61d4ec4aaf8c781d35d2da07dbeb7903
ArielSP-King <arielaespibot@gmail.com>
2022-08-19 11:24:22 +0700

    Ariel-Baruna

---
+++ connect/language/help.js
@@ -0,0 +1,216 @@
+exports.help = (prefix, author, botname, owner) => {
+return `✘ *I N F O - B O T*
+
+⌕ Nama Owner : *Ariel SP*
+⌕ Nama Bot : *${global.botname}*
+⌕ Nomor Owner : wa.me/${global.premium}
+
+✘ *G R O U P - M E N U*
+
+⌕ add
+⌕ kick
+⌕ promote
+⌕ demote
+⌕ setname
+⌕ setdesc
+⌕ setppgrup
+⌕ tagall
+⌕ hidetag
+⌕ group
+⌕ editinfo
+⌕ antilink
+
+✘ *O W N E R - M E N U*
+
+⌕ join
+⌕ creategc
+⌕ block
+⌕ unblock
+⌕ bcall
+⌕ bcgc
+⌕ broadcast
+
+✘ *F U N - M E N U*
+
+⌕ jadian
+⌕ jodohku
+⌕ tictactoe
+⌕ delttt
+⌕ family100
+⌕ tebak
+⌕ math
+⌕ suitpvp
+⌕ apakah
+⌕ rate
+⌕ bisakah
+⌕ bagaimanakah
+⌕ cekganteng
+⌕ cekcantik
+⌕ ceksange
+⌕ cekgay
+⌕ ceklesbi
+⌕ kapankah
+⌕ cekmati
+⌕ orangpintar
+⌕ orangbego
+⌕ anakbuangan
+⌕ couple
+⌕ pinterest
+⌕ happymod
+⌕ quotes
+⌕ gombalan
+
+✘ *C O N V E R T - M E N U*
+
+⌕ tahta
+⌕ teksto
+⌕ sticker
+⌕ tomp3
+⌕ tomp4
+⌕ toimage
+⌕ stickerwm
+⌕ stickermeme
+⌕ emojimix
+⌕ emojimix2
+
+✘ *N U L I S - M E N U*
+
+⌕ nulis
+⌕ nuliskiri
+⌕ nuliskanan
+⌕ foliokiri
+⌕ foliokanan
+
+✘ *T E X T P R O - M E N U*
+
+⌕ arielpetir <text>
+⌕ arielpetir2 <text>
+⌕ pottery <text>
+⌕ bussines3d <text>
+⌕ stoneariel3d <text>
+⌕ neon3dariel <text>
+⌕ neon <text>
+⌕ impresiveariel <text>
+⌕ neondevil3dariel <text>
+⌕ underwater3d <text>
+⌕ bears3dariel <text>
+⌕ fireworkbucin <text>
+⌕ metaldarkariel <text>
+⌕ halloweenariel <text>
+⌕ captainamerikabyariel <text>
+⌕ spacetext <text>
+⌕ plastic3d <text>
+⌕ dropwater3d <text>
+⌕ avengerslogo3d <text>
+⌕ brokenglass <text>
+⌕ wonderfulgravity3d <text>
+⌕ futuristicneon3d <text>
+⌕ skytext3d <text>
+⌕ robotr2 <text>
+⌕ pornhub teks1|teks2
+⌕ breakwall <text>
+⌕ matrixstyle <text>
+⌕ waterpiperariel <text>
+⌕ blackpink <text>
+⌕ lightglow <text>
+⌕ glass <text>
+⌕ hoorror_blood <text>
+⌕ sand <text>
+⌕ sketch <text>
+⌕ magma <text>
+⌕ batman <text>
+⌕ demon <text>
+⌕ sci_fi <text>
+⌕ sea_metal <text>
+⌕ skeleton <text>
+⌕ transformer <text>
+⌕ warning <text>
+⌕ denim <text>
+
+✘ *A N I M E - M E N U*
+
+⌕ manga
+⌕ otakudesu
+⌕ megumin
+⌕ waifu
+⌕ neko
+⌕ shinobu
+⌕ awoo
+⌕ husbu
+⌕ neko2
+
+✘ *KISAH - M E N U*
+
+⌕ ${prefix}kisahnabi adam
+⌕ ${prefix}kisahnabi ayyub
+⌕ ${prefix}kisahnabi daud
+⌕ ${prefix}kisahnabi dzulkifli
+⌕ ${prefix}kisahnabi harun
+⌕ ${prefix}kisahnabi hud
+⌕ ${prefix}kisahnabi ibrahim
+⌕ ${prefix}kisahnabi idris
+⌕ ${prefix}kisahnabi ilyas
+⌕ ${prefix}kisahnabi ilyasa
+⌕ ${prefix}kisahnabi isa
+⌕ ${prefix}kisahnabi ishaq
+⌕ ${prefix}kisahnabi ismail
+⌕ ${prefix}kisahnabi luth
+⌕ ${prefix}kisahnabi muhammad
+⌕ ${prefix}kisahnabi musa
+⌕ ${prefix}kisahnabi nuh
+⌕ ${prefix}kisahnabi sholeh
+⌕ ${prefix}kisahnabi sulaiman
+⌕ ${prefix}kisahnabi syuaib
+⌕ ${prefix}kisahnabi yahya
+⌕ ${prefix}kisahnabi yaqub
+⌕ ${prefix}kisahnabi yunus
+⌕ ${prefix}kisahnabi yusuf
+⌕ ${prefix}kisahnabi zakariya
+
+✘ *D O W N L O A D - M E N U*
+
+⌕ play
+⌕ ytmp3
+⌕ ytmp4
+⌕ tiktoknowm
+⌕ tiktokmp3
+⌕ mediafire
+
+✘ *W A - I M U N*
+
+⌕ waslayer
+⌕ waiphonev1
+⌕ waiphonev2
+
+✘ *S O U N D - M E N U*
+
+⌕ sound1
+⌕ sound2
+⌕ sound3
+⌕ sound4
+⌕ sound5
+⌕ sound6
+⌕ sound7
+⌕ sound8
+⌕ sound9
+⌕ sound10
+⌕ sound11
+⌕ sound12
+⌕ sound13
+⌕ sound14
+⌕ sound15
+⌕ sound16
+⌕ sound17
+⌕ sound18
+⌕ sound19
+⌕ sound20
+⌕ sound21
+⌕ sound22
+⌕ sound23
+⌕ sound24
+⌕ sound25
+⌕ sound26
+⌕ sound27
+⌕ sound28
+⌕ sound29`
+}
